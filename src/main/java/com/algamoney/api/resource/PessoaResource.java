0c64cebc32d62b25e303d14d66c9c39af27097e1
romario <romario.adsistemas@gmail.com>
2020-11-02 20:56:17 -0300

    aplicacao rest para crud

--- src/main/java/com/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algamoney/api/resource/PessoaResource.java
@@ -65,4 +65,10 @@ public class PessoaResource {
         return ResponseEntity.ok(pessoaSalva);
 
     }
+
+    @PutMapping("/{codigo}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+        PessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+    }
 }
--- src/main/java/com/algamoney/api/service/PessoaService.java
+++ src/main/java/com/algamoney/api/service/PessoaService.java
@@ -15,11 +15,23 @@ public class PessoaService {
     private PessoaRepository pessoaRepository;
 
     public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+        Pessoa pessoaSalva = encontrarPessoa(codigo);
+
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+        return pessoaRepository.save(pessoaSalva);
+    }
+
+    public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+        Pessoa pessoaSalva = encontrarPessoa(codigo);
+        pessoaSalva.setAtivo(ativo);
+        pessoaRepository.save(pessoaSalva);
+    }
+
+    private Pessoa encontrarPessoa(Long codigo) {
         Pessoa pessoaSalva = pessoaRepository.getOne(codigo);
         if (pessoaSalva == null) {
             throw new EmptyResultDataAccessException(1);
         }
-        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-        pessoaRepository.save(pessoaSalva);
+        return pessoaSalva;
     }
 }
