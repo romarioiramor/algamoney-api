14904b3c1656cd37e2f9b4bf66d2c424dde3b2ad
romario <romario.adsistemas@gmail.com>
2020-11-02 21:24:50 -0300

    criando migracao e tabela lancamento

--- src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
+++ src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -1,5 +1,12 @@
 CREATE TABLE pessoa (
     codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    ativo VARCHAR(50) NOT NULL
+    nome VARCHAR(50) NOT NULL,
+    logradouro VARCHAR(50),
+    numero VARCHAR(50),
+    complemento VARCHAR(50),
+    bairro VARCHAR(50),
+    cep VARCHAR(50),
+    cidade VARCHAR(50),
+    estado VARCHAR(50),
+    ativo BOOLEAN NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
